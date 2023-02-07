c043df2bd0fd9e0ce669fd50a60fec3514e4d631
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2023-02-07 20:19:21 -0300

    Add animation to logo

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,39 +1,14 @@
 import './index.scss';
 import LogoI from '../../../assets/images/logo-i.png';
-import { useEffect, useRef } from 'react';
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
-import gsap from 'gsap-trial';
+// import { useEffect, useRef } from 'react';
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
+// import gsap from 'gsap-trial';
 
 const Logo = () => {
 
-    const  bgRef = useRef();
-    const  solidLogoRef = useRef();
-
-    useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin);
-
-        gsap.timeline()
-        .to(bgRef.current, {
-            duration: 1,
-            opacity: 1,
-        })
-    
-        gsap.fromTo(
-          solidLogoRef.current,
-          {
-            opacity: 0,
-          },
-          {
-            opacity: 1,
-            delay: 4,
-            duration: 4,
-          }
-        )
-    }, []);
-
     return (
-        <div className='logo-container' ref={bgRef}>
-            <img ref={solidLogoRef} className='solid-logo' src={LogoI} alt='I'/>
+        <div className='logo-container'>
+            <img className='solid-logo' src={LogoI} alt='I'/>
         </div>
     )
 }
